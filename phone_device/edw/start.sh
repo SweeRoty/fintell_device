c769d3c0347bc2ada3ec882366dfb1db2aefe3e5
SweeRoty <sweeroty@gmail.com>
2021-03-29 10:49:53 +0800

    add tables of step 2 and 3

--- phone_device/edw/start.sh
+++ phone_device/edw/start.sh
@@ -11,7 +11,35 @@ partitioned by (data_date string)
 row format serde
 	'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
 stored as orc;
-
 describe tmp.step1_phone;
+
+create table if not exists step2_sample (phone_salt string, imei string, itime int, source string, min_itime int)
+partitioned by (data_date string)
+row format serde
+	'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+stored as orc;
+describe tmp.step2_sample;
+
+create table if not exists step3_feature (
+	record_count_in_365 int, 
+	device_count_in_365 int,
+	source_count_in_365 int,
+	record_count_in_180 int, 
+	device_count_in_180 int,
+	source_count_in_180 int,
+	record_count_in_90 int, 
+	device_count_in_90 int,
+	source_count_in_90 int,
+	record_count_in_30 int, 
+	device_count_in_30 int,
+	source_count_in_30 int,
+	record_count_in_7 int, 
+	device_count_in_7 int,
+	source_count_in_7 int)
+partitioned by (data_date string)
+row format serde
+	'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+stored as orc;
+describe tmp.step3_feature;
 "
 beeline -e "$job" > log_create_tmp_table
